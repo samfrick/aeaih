07936bc68f17268aad94d0335414affb4edd79da
FRICK SALOME <salome.frick@etu.unistra.fr>
2025-10-30 19:37:46 +0100

    update page

--- page.php
+++ page.php
@@ -1,11 +1,1 @@
-<?php
-// Exit if accessed directly.
-defined('ABSPATH') || exit;
-
-get_header();
-?>
-
-<?php // include __DIR__ . '/flex/flex.php' ?>
-
-<?php
-get_footer();
+<?php get_template_part('templates/page');
---
+++ templates/page.php
@@ -0,0 +1,11 @@
+<?php
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+get_header();
+?>
+
+<?php // include __DIR__ . '/flex/flex.php' ?>
+
+<?php
+get_footer();
