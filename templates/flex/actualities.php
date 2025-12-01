3d4a8443de62a4a32cd9d30c865f68f7239138c7
FRICK SALOME <salome.frick@etu.unistra.fr>
2025-12-01 16:37:29 +0100

    update actualités

--- templates/flex/actualities.php
+++ templates/flex/actualities.php
@@ -9,14 +9,6 @@ $actualities = get_field('actualities');
     <?php if ($actualities['actu_subtitle']) : ?>
         <p class="text-xl"><?= esc_html($actualities['actu_subtitle']) ?></p>
     <?php endif; ?>
-    <div class="actu-content flex flex-col items-center bg-white/10 backdrop-blur-sm border rounded-lg border-white/30 shadow-lg p-6 w-1/5 my-6">
-        <?php if ($actualities['actu_image']) : ?>
-            <img class="w-30 aspect-square" src="<?= esc_url($actualities['actu_image']['url']) ?>" alt="<?= esc_attr($actualities['actu_image']['alt']) ?>">
-        <?php endif; ?>
-        <?php if ($actualities['actu_description']) : ?>
-            <p class="text-"><?= esc_html($actualities['actu_description']) ?></p>
-        <?php endif; ?>
-    </div>
     <?php
     $btn_actu = $actualities['actu_btn_events'] ?? null;
     if ($btn_actu) :
