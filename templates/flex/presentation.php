1962f996101f8f0815794c9483868336d7ff3546
FRICK SALOME <salome.frick@etu.unistra.fr>
2025-12-01 16:37:48 +0100

    update présentation

--- templates/flex/presentation.php
+++ templates/flex/presentation.php
@@ -2,23 +2,23 @@
 $presentation = get_field('presentation');
 ?>
 
-<section id="presentation" class="presentation flex flex-row items-center justify-center py-16">
+<section id="presentation" class="presentation grid sm:grid-cols-2 items-center justify-center gap-12 py-16 m-32">
     <?php if ($presentation['pres_image']) : ?>
-        <img class="w-100 object-cover" src="<?= esc_url($presentation['pres_image']['url']) ?>" alt="<?= esc_attr($presentation['pres_image']['alt']) ?>">
+        <img class="object-cover" src="<?= esc_url($presentation['pres_image']['url']) ?>" alt="<?= esc_attr($presentation['pres_image']['alt']) ?>">
     <?php endif; ?>
     <div>
         <?php if ($presentation['pres_presentation']) : ?>
-            <p class="text-"><?= esc_html($presentation['pres_presentation']) ?></p>
+            <p><?= esc_html($presentation['pres_presentation']) ?></p>
         <?php endif; ?>
         <?php
         $btn_pres = $presentation['pres_btn_members'] ?? null;
         if ($btn_pres) :
             $page_id = url_to_postid($btn_pres);
-                     if ($page_id) {
-                         $titre_pres = get_the_title($page_id);
-                     }
+            if ($page_id) {
+                $titre_pres = get_the_title($page_id);
+            }
         ?>
-            <a href="<?= esc_url($btn_pres) ?>" class="btn">En savoir plus</a>
+            <a href="<?= esc_url($btn_pres) ?>" class="btn">Découvrir les membres</a>
         <?php endif; ?>
     </div>
 </section>
